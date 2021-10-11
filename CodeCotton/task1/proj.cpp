1a9219f318acdf412a6363927112df5d564bf717
AhmedBakryCode <92317148+AhmedBakryCode@users.noreply.github.com>
2021-10-11 16:52:29 +0200

    code of tasks
    new code

---
+++ CodeCotton/task1/proj.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std ;
+int main()
+{
+    int a=8;
+    int b=5;
+    int c=a*b;
+    int d=(a+b)*2;
+    int e=6;
+    int f=e*e;
+    int g=e*4;
+    cout<<"rectangle area="<<c;
+    cout<<"perimeter of rectangle="<<d;
+    cout<<"sqare area="<<f;
+    cout<<"perimeter square="<<g;
+    return 0; 
+
+}
